cb01dd07d20aef34c23d27de4d0ff9c1ee94c2c8
Nikita <almostroll@yandex.ru>
2020-11-24 21:24:08 +0300

    Add ability to get resources

--- src/Application.kt
+++ src/Application.kt
@@ -3,6 +3,7 @@ package ru.neexol.debtable
 import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
@@ -15,25 +16,8 @@ fun Application.module() {
         header("Access-Control-Allow-Origin", "*")
     }
     routing {
-        route("/text") {
-            get {
-                val textResponse = "Hello world!"
-                call.respondText(textResponse)
-            }
-            post("/{text}") {
-                val textParameter = call.parameters["text"]!!
-                call.respondText(textParameter)
-            }
-        }
-        route("/json") {
-            get {
-                val rawJsonResponse = """{"text":"Hello world!"}"""
-                call.respondText(rawJsonResponse, ContentType.Application.Json)
-            }
-            post {
-                val bodyJson = call.receiveText()
-                call.respondText(bodyJson, ContentType.Application.Json)
-            }
+        static {
+            resources("front")
         }
     }
 }
