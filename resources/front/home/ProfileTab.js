259cc69c1eac2d2be13fa8dccf827a07b7d431b9
Vladimir <vovcrane@mai.ru>
2020-12-07 10:49:48 +0300

    Improve login view on profile page

--- resources/front/home/ProfileTab.js
+++ resources/front/home/ProfileTab.js
@@ -7,6 +7,7 @@ class ProfileTab extends React.Component {
     };
 
     handleDisplayNameChange = e => this.setState({displayName: e.target.value});
+
     handleSaveClick = e => {
         sendPatch(ROUTE_CHANGE_NAME, JSON.stringify({
             new_display_name: this.state.displayName
@@ -23,7 +24,9 @@ class ProfileTab extends React.Component {
             }
         });
     }
+
     handleChangePassClick = e => {}
+
     handleLogOut = e => {
         setJWT(undefined);
         redirectToLogin();
@@ -31,29 +34,33 @@ class ProfileTab extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <h1>Профиль (<strong>{this.props.profile.username})</strong></h1>
+            <>
+                <div className="container">
+                    <h1>{this.props.profile.username}</h1>
 
-                <label htmlFor="display_name"><b>Имя пользователя</b></label>
-                <div style={{display: "flex"}}>
-                    <input type="text" placeholder="Введите имя" name="display_name" id="display_name"
-                           style={{flexGrow: "1"}}
-                           value={this.state.displayName}
-                           onChange={this.handleDisplayNameChange}/>
-                    <button type="submit" className="apply-btn"
-                            onClick={this.handleSaveClick}
-                            disabled={this.state.displayName === this.props.profile.display_name}
-                    >Сохранить</button>
+                    <label htmlFor="display_name"><b>Имя пользователя</b></label>
+                    <div style={{display: "flex"}}>
+                        <input type="text" placeholder="Введите имя" name="display_name" id="display_name"
+                               style={{flexGrow: "1"}}
+                               value={this.state.displayName}
+                               onChange={this.handleDisplayNameChange}/>
+                        <button type="submit" className="apply-btn"
+                                onClick={this.handleSaveClick}
+                                disabled={this.state.displayName === this.props.profile.display_name}
+                        >Сохранить</button>
+                    </div>
+
+                    <button className="apply-btn" onClick={this.handleChangePassClick}>
+                        Сменить пароль
+                    </button>
+
+                    <button className="apply-btn" onClick={this.handleLogOut}>
+                        Выйти из этой параши
+                    </button>
                 </div>
 
-                <button className="apply-btn" onClick={this.handleChangePassClick}>
-                    Сменить пароль
-                </button>
 
-                <button className="apply-btn" onClick={this.handleLogOut}>
-                    Выйти из этой параши
-                </button>
-            </div>
+            </>
         );
     }
 }
