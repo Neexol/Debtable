ff74bc921c9e84fafcf39049acc141eb39d351f8
Vladimir <vovcrane@mai.ru>
2020-12-07 10:19:17 +0300

    Some refactor in utils

--- resources/front/utils/utils.js
+++ resources/front/utils/utils.js
@@ -15,26 +15,18 @@ const ROUTE_ROOM = id => 'api/rooms/' + id;
 
 const Loader = () => <div className="loader"/>;
 
-function getJWT() {
+const getJWT = () => {
     let matches = document.cookie.match(new RegExp("(?:^|; )jwt_auth=([^;]*)"));
     return matches ? decodeURIComponent(matches[1]) : undefined;
 }
 
-function setJWT(jwt) {
-    // if (jwt === undefined) {
-    //     document.cookie = 'jwt_auth=;max-age=-1';
-    // } else {
-    //     document.cookie = 'jwt_auth='+encodeURIComponent(jwt)+';path=/';
-    // }
-    document.cookie = 'jwt_auth=' + (jwt === undefined
+const setJWT = jwt => document.cookie = 'jwt_auth=' + (
+    jwt === undefined
         ? ';max-age=-1'
         : encodeURIComponent(jwt)
-    ) + ';path=/';
-}
+) + ';path=/';
 
 const redirectToLogin = () => location.replace(HOST_URL+'auth/login.html');
-
-// const sendRequest = (path, options) => $.ajax(HOST_URL+path, options);
 
 const sendRequest = (path, method, body, onSuccess, onError) => $.ajax(HOST_URL+path, {
     type: method,
@@ -50,47 +42,5 @@ const sendPost   = (path, body, onSuccess, onError) => sendRequest(path, 'POST',
 const sendPatch  = (path, body, onSuccess, onError) => sendRequest(path, 'PATCH',  body, onSuccess, onError);
 const sendDelete = (path, body, onSuccess, onError) => sendRequest(path, 'DELETE', body, onSuccess, onError);
 const sendPut    = (path, body, onSuccess, onError) => sendRequest(path, 'PUT',    body, onSuccess, onError);
-
-// function sendPost(path, body, onSuccess, onError) {
-//     sendRequest(path, {
-//         type: "POST",
-//         data: body,
-//         contentType: "application/json",
-//         headers: { Authorization: `Bearer ${getJWT()}` },
-//         success: onSuccess,
-//         error: onError
-//     });
-// }
-//
-// function sendPatch(path, body, onSuccess, onError) {
-//     sendRequest(path, {
-//         type: "PATCH",
-//         data: body,
-//         contentType: "application/json",
-//         headers: { Authorization: `Bearer ${getJWT()}` },
-//         success: onSuccess,
-//         error: onError
-//     });
-// }
-//
-// function sendDelete(path, body, onSuccess, onError) {
-//     sendRequest(path, {
-//         type: "DELETE",
-//         data: body,
-//         contentType: "application/json",
-//         headers: { Authorization: `Bearer ${getJWT()}` },
-//         success: onSuccess,
-//         error: onError
-//     });
-// }
-
-// function sendGet(path, onSuccess, onError) {
-//     sendRequest(path, {
-//         type: "GET",
-//         headers: { Authorization: `Bearer ${getJWT()}` },
-//         success: onSuccess,
-//         error: onError
-//     });
-// }
 
 
